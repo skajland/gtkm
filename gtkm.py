fca05892aaa7877419e0a25e79e680b0890cbd7a
SnoopyPlayz <maciej.kubis@teb.edu.pl>
2023-07-07 21:37:51 +0200

    com

--- gtkm.py
+++ gtkm.py
@@ -11,5 +11,5 @@ while 1:
     accumulator += currentTime-lastFrame
     lastFrame = currentTime
 
-    if (accumulator >= timePerFrame):
+    if accumulator >= timePerFrame:
         accumulator -= timePerFrame
