0ac4275ce896657ad89cff5d82c17182db942e33
skajland <skajlandplays@gmail.com>
2023-07-07 22:00:49 +0200

    zombie script

--- gtkm.py
+++ gtkm.py
@@ -1,22 +1,19 @@
 # GTKM GAME JAM SNOOPY AND SKAJLAND
 from zombie import Zombie
 import pygame
-import sys
 import time
-
+import sys
 screen = pygame.display.set_mode((800, 800))  # Creates the window
-
-
 lastFrame = time.time_ns()
 timePerFrame = 16666667
 accumulator = 0
 zombie1 = Zombie((400, 400))
+
 while 1:
     currentTime = time.time_ns()
 
     accumulator += currentTime - lastFrame
     lastFrame = currentTime
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -26,8 +23,4 @@ while 1:
                 zombie1.move()
 
     if accumulator >= timePerFrame:  # UPDATE I RENDER
-        screen.fill('White')
-        zombie1.render(screen)
-    if accumulator >= timePerFrame:  # UPDATE I RENDER
         accumulator -= timePerFrame
-        pygame.display.update()
