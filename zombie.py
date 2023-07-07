28d17f724f7e8aa3a0397b624e15a04ad70ad656
skajland <skajlandplays@gmail.com>
2023-07-07 21:40:38 +0200

    zombie script

--- zombie.py
+++ zombie.py
@@ -2,16 +2,21 @@ import pygame
 
 
 class Zombie:
+    # loads the image
     zombie = pygame.image.load("images/Zombie.png")
+    # gets the rect of the image
     zombie_rect = zombie.get_rect()
     start_pos = ()
 
     def __init__(self, start_pos):
+        # start position
         self.start_pos = start_pos
         self.zombie_rect.y, self.zombie_rect.x = start_pos
 
     def move(self):
+        # Moves the zombie left
         self.zombie_rect.x -= 1
 
     def render(self, display):
+        # Renders the zombie
         display.blit(self.zombie, self.zombie_rect)
