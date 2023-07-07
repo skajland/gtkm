269e9272c6f5c3f61efba8493a4a60d027988677
SnoopyPlayz <maciej.kubis@teb.edu.pl>
2023-07-07 21:30:43 +0200

    com

--- gtkm.py
+++ gtkm.py
@@ -1,7 +1,6 @@
+# GTKM GAME JAM SNOOPY AND SKAJLAND
 import pygame
 import time
-# GTKM GAME JAM SNOOPY AND SKAJLAND
-timePerFrame = 1000000000. / 60
 lastFrame = time.time_ns()
 now = 16666667
 accumulator = 0
@@ -12,6 +11,5 @@ while 1:
     accumulator += currentTime-lastFrame
     lastFrame = currentTime
 
-    if (accumulator >= now)
-
+    if (accumulator >= now):
         accumulator -= now
