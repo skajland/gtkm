0facacad55368fb7f66defc399ab789a5124167c
SnoopyPlayz <maciej.kubis@teb.edu.pl>
2023-07-08 21:29:03 +0200

    com

--- bullet.py
+++ bullet.py
@@ -11,20 +11,48 @@ class Bullet:
         self.bullet_rect = self.bullet_img.get_rect()
         self.bullet_rect.x = x
         self.bullet_rect.y = y
-    def ray(self,blocks ,screen):
-        for i in range(5):
-            radians = math.radians(20)#MAKE COMP(INTER) TIME
+    def rotpos(self, i , rad):
+        radians = math.radians(rad)  # MAKE COMP(INTER) TIME
 
-            x = self.bullet_rect.x + math.sin(radians) *50 * i
-            y = self.bullet_rect.y - math.cos(radians) *50 * i
+        x = self.bullet_rect.x + math.sin(radians) * 50 * i
+        y = self.bullet_rect.y - math.cos(radians) * 50 * i
+        return x,y
+
+    def ray(self,blocks ,screen):
+        hitF = False
+        hitL = False
+        hitR = False
+
+        for i in range(7):
+            xL,yL = self.rotpos(i, 20)
+            x0,y0 = self.rotpos(i, 0)
+            xR,yR = self.rotpos(i, -20)
 
             for block in blocks:
-                if block.block_rect.collidepoint((x, y)):
-                    print("COL RAY")
-                screen.blit(self.bullet_img,(x,y))
+
+                if block.block_rect.collidepoint((x0, y0)):
+                    print("t")
+                    hitF=True
+                if block.block_rect.collidepoint((xL, yL)):
+                    hitL=True
+                if block.block_rect.collidepoint((xR, yR)):
+                    hitR = True
+
+                screen.blit(self.bullet_img,(xL,yL))
+                screen.blit(self.bullet_img,(xR,yR))
+                screen.blit(self.bullet_img,(x0,y0))
+
+        self.bullet_rect.y -= 1
+        if not hitF:
+            return
+        if not hitL:
+            self.bullet_rect.x +=1
+            return
+        if not hitR:
+            self.bullet_rect.x -= 1
+            return
 
     def update(self,blocks, screen):
-        self.bullet_rect.y -= 1
         self.ray(blocks,screen)
     def render(self, screen):
         screen.blit(self.bullet_img, self.bullet_rect)  # Renders the object
