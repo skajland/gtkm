791e7c37db7822c47a7df1441a89ebd185498cf7
skajland <skajlandplays@gmail.com>
2023-07-08 13:47:02 +0200

    turret and blocks

--- block.py
+++ block.py
@@ -2,8 +2,10 @@ import pygame
 
 
 class Block:
-    block_img = pygame.image.load("res/TestBlock.png")
-    block_rect = block_img.get_rect()
+
+    def __init__(self):
+        self.block_img = pygame.image.load("res/TestBlock.png")
+        self.block_rect = self.block_img.get_rect()
 
     def render(self, screen):
         screen.blit(self.block_img, self.block_rect)
--- gtkm.py
+++ gtkm.py
@@ -7,6 +7,7 @@ from turret import Turret
 import pygame
 import time
 import sys
+
 screen = pygame.display.set_mode((800, 800))  # Creates the window
 lastFrame = time.time_ns()
 timePerFrame = 16666667
@@ -15,10 +16,10 @@ turret1 = Turret((400, 400))
 
 blocks = []
 
-all_bullets = [bullet(200,200)]
+all_bullets = [bullet(200, 200)]
+
 
 def update():
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -33,12 +34,12 @@ def render():
     screen.fill('darkgray')
     i = 0
     while i < len(all_bullets):
-    #    print(all_bullets[i].y)
-        bullet.render(all_bullets[i],screen)
-        i+=1
+        bullet.render(all_bullets[i], screen)
+        i += 1
+
     for block in blocks:
-        print(block.block_rect)
         block.render(screen)
+
     turret1.render(screen)
     pygame.display.update()
 
