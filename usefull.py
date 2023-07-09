19c6002a48740262cda84bc8d606a8317e680e2d
SnoopyPlayz <maciej.kubis@teb.edu.pl>
2023-07-09 15:15:11 +0200

    com

--- usefull.py
+++ usefull.py
@@ -8,8 +8,9 @@ all_blocks = (
     ("res/Brick.png", (96, 96)), ("res/Vase.png", (19 * 3.2, 31 * 3.2)),
     ("res/wiatrak/wiatrak1.png", (19 * 3.2, 31 * 4)))
 
-blocks = [Block(all_blocks[0][0], all_blocks[0][1])]
-
+blocks = [Block(all_blocks[0][0], (100,912)), Block(all_blocks[0][0], (200,912))]
+blocks[0].block_rect.x = -100
+blocks[1].block_rect.x = +950
 def game_exit():
     pygame.quit()
     sys.exit()
