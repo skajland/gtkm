81bf1d10f13ee404be0f4b9cbc427631a2a73ae5
skajland <skajlandplays@gmail.com>
2023-07-09 17:25:16 +0200

    fix

--- usefull.py
+++ usefull.py
@@ -1,13 +1,14 @@
 import pygame
 import sys
+import waves
 from block import Block
 from bullet import Bullet
 game_state = "Menu"
 
 
 all_blocks = (
-    ("res/Brick.png", (96, 96), 50), ("res/Vase.png", (19 * 3.2, 31 * 3.2), 25),
-    ("res/wiatrak/wiatrak1.png", (19 * 3.2, 31 * 4), 75))
+    ("res/Brick.png", (96, 96), 75, 15), ("res/Vase.png", (19 * 3.2, 31 * 3.2), 25, 5),
+    ("res/wiatrak/wiatrak1.png", (19 * 3.2, 31 * 4), 50, 10))
 
 
 all_bullets = [Bullet(200, 800)]
@@ -24,6 +25,8 @@ def game_exit():
 
 def play_again(state):
     global game_state
+    waves.waves = 0
+    waves.coins = waves.coins_default
     all_bullets.clear()
     blocks.clear()
     print(state[0])
---
+++ waves.py
@@ -0,0 +1,16 @@
+import pygame
+wave = 0
+coins_default = 50
+coins = coins_default
+font = pygame.font.Font(None, 64)
+
+
+def render(screen):
+    rendered_font = font.render(str(coins), 1, 'Black')
+    font_rect = rendered_font.get_rect()
+    font_rect.center = (screen.get_width() - 120, screen.get_height() / 2 - 350)
+    screen.blit(rendered_font, font_rect)
+    coins_img = pygame.image.load("res/Coins.png")
+    coins_img_rect = coins_img.get_rect()
+    coins_img_rect.midleft = (screen.get_width() - 100, screen.get_height() / 2 - 350)
+    screen.blit(coins_img, coins_img_rect)
